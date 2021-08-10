7d79921e113832e516793c2e5df36968ae598e6c
Guillermo Ramirez Garcia <memo@hpe.com>
2021-08-10 11:28:23 +0900

    argocd implementation 2

--- env/tokyo-ckd/install.sh
+++ env/tokyo-ckd/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 kubectl -n postgres-operator create secret generic postgres-operator-etcd-root-password --from-literal=etcd-root-password=CHANGEME
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+helm repo pull 
