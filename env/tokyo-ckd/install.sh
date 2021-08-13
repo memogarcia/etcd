57cffd01621a250ef177b9b2e7b64607c5a7e579
Guillermo Ramirez Garcia <memo@hpe.com>
2021-08-13 13:10:54 +0900

    argocd implementation 10

--- env/tokyo-ckd/install.sh
+++ env/tokyo-ckd/install.sh
@@ -2,6 +2,8 @@
 
 kubectl -n postgres-operator create secret generic postgres-operator-etcd-root-password --from-literal=etcd-root-password=CHANGEME
 
+kubectl -n etcd-argo create secret generic postgres-operator-etcd-root-password --from-literal=etcd-root-password=CHANGEME
+
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install etcd bitnami/etcd
@@ -12,3 +14,5 @@ helm pull bitnami/etcd
 # do I need nexus for this?
 
 helm template etcd bitnami/etcd  -f env/tokyo-ckd/versions/6.3.3/values.yaml
+
+helm install etcd bitnami/etcd -n etcd-argo -f values.yml
